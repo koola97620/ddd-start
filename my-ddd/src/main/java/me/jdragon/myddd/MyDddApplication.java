4a7d47dfa25cf2216dbdda4fdb25044af572e919
Jaeyong <koola97620@nate.com>
2019-04-19 22:32:03 +0900

    Chapter 01

---
+++ my-ddd/src/main/java/me/jdragon/myddd/MyDddApplication.java
@@ -0,0 +1,13 @@
+package me.jdragon.myddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDddApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyDddApplication.class, args);
+  }
+
+}
