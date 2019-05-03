d284e5d284517776a9ee76c0269411097693bdbe
Jaeyong <koola97620@nate.com>
2019-05-03 23:10:09 +0900

    Chapter 01

--- my-ddd/src/main/java/me/jdragon/myddd/domain/Order.java
+++ my-ddd/src/main/java/me/jdragon/myddd/domain/Order.java
@@ -8,19 +8,30 @@ import java.util.List;
  * @description
  */
 public class Order {
+  private String orderNumber;
   private OrderState state;
   private ShippingInfo shippingInfo;
   private List<OrderLine> orderLines;
   private Money totalAmounts;
 
-  public Order(List<OrderLine> orderLines) {
+  public Order(List<OrderLine> orderLines , ShippingInfo shippingInfo) {
     setOrderLines(orderLines);
+    setShippingInfo(shippingInfo);
   }
+
   public void setOrderLines(List<OrderLine> orderLines) {
     verifyAtLeastOneOrMoreOrderLines(orderLines);
     this.orderLines = orderLines;
     calculateTotalAmounts();
   }
+
+  public void setShippingInfo(ShippingInfo shippingInfo) {
+    if(shippingInfo == null) {
+      throw new IllegalArgumentException("no ShippingInfo");
+    }
+    this.shippingInfo = shippingInfo;
+  }
+
 
   private void verifyAtLeastOneOrMoreOrderLines(List<OrderLine> orderLines) {
     if(orderLines == null || orderLines.isEmpty()) {
@@ -40,9 +51,42 @@ public class Order {
 //    this.shippingInfo = newShippingInfo;
 //  }
 
-  public boolean isShippingChangeable() {
-    return state == OrderState.PAYMENT_WAITING || state == OrderState.PREPARING;
+//  public boolean isShippingChangeable() {
+//    return state == OrderState.PAYMENT_WAITING || state == OrderState.PREPARING;
+//  }
+
+  public void changeShippingInfo(ShippingInfo newShippingInfo) {
+    verifyNotYetShipped();
+    setShippingInfo(newShippingInfo);
+  }
+
+  public void cancel() {
+    verifyNotYetShipped();
+    this.state = OrderState.CANCELED;
+  }
+
+  public void verifyNotYetShipped() {
+    if(state != OrderState.PAYMENT_WAITING && state != OrderState.PREPARING) {
+      throw new IllegalArgumentException("already shipped");
+    }
   }
 
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((orderNumber == null) ? 0 : orderNumber.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if(this == obj) return true;
+    if(obj == null) return false;
+    if(obj.getClass() != Order.class) return false;
+    Order other = (Order)obj;
+    if(this.orderNumber == null) return false;
+    return this.orderNumber.equals(other.orderNumber);
+  }
 }
