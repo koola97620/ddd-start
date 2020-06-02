1e45f6891c74c3e43bb242a3bdbc5078fbd6614b
Jaeyong <koola97620@nate.com>
2020-06-03 00:32:52 +0900

    Chapter 01

---
+++ my-ddd/src/main/java/me/jdragon/chapter1/Order.java
@@ -0,0 +1,13 @@
+package me.jdragon.chapter1;
+
+
+/**
+ * @author choijaeyong on 2020/06/03.
+ * @project my-ddd
+ * @description
+ */
+public class Order {
+  private OrderState state;
+  private ShippingInfo shippingInfo;
+
+}
