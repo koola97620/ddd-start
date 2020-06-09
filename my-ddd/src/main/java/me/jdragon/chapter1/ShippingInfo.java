2970e93ccea0618fc830ba626e6f4be5970703ef
Jaeyong <koola97620@nate.com>
2020-06-09 23:28:24 +0900

    Chapter 01

--- my-ddd/src/main/java/me/jdragon/chapter1/ShippingInfo.java
+++ my-ddd/src/main/java/me/jdragon/chapter1/ShippingInfo.java
@@ -8,11 +8,13 @@ package me.jdragon.chapter1;
 public class ShippingInfo {
 
   // 개념적으로 받는 사람을 의미한다
-  private String receiverName;
-  private String receiverPhoneNumber;
+//  private String receiverName;
+//  private String receiverPhoneNumber;
+  private Receiver receiver;
 
   // 개념적으로 주소를 나타낸다.
-  private String shippingAddress1;
-  private String shippingAddress2;
-  private String shippingZipcode;
+//  private String shippingAddress1;
+//  private String shippingAddress2;
+//  private String shippingZipcode;
+  private Address address;
 }
