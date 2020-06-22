f9f6f672b917d785d3c68a624411f8a17284fded
Jaeyong <koola97620@nate.com>
2020-06-22 23:37:20 +0900

    Chapter 04

---
+++ my-ddd/src/main/java/me/jdragon/chapter3/EmailSet.java
@@ -0,0 +1,16 @@
+package me.jdragon.chapter3;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.validation.constraints.Email;
+
+/**
+ * @author choijaeyong on 2020/06/22.
+ * @project my-ddd
+ * @description
+ */
+public class EmailSet {
+  private Set<Email> emails = new HashSet<>();
+
+
+}
