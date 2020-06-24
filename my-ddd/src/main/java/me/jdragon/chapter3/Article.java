533fab7ad0ef8eb256c8b3b98bbbc9f2f4c38446
Jaeyong <koola97620@nate.com>
2020-06-25 00:01:47 +0900

    Chapter 04

--- my-ddd/src/main/java/me/jdragon/chapter3/Article.java
+++ my-ddd/src/main/java/me/jdragon/chapter3/Article.java
@@ -1,7 +1,13 @@
 package me.jdragon.chapter3;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SecondaryTable;
+import javax.persistence.Table;
 
 /**
  * @author choijaeyong on 2020/06/24.
@@ -10,8 +16,20 @@ import javax.persistence.Id;
  */
 
 @Entity
+@Table(name = "article")
+@SecondaryTable(
+    name = "article_content", pkJoinColumns = @PrimaryKeyJoinColumn(name="id")
+)
 public class Article {
   @Id
   private Long id;
+
+  private String title;
+
+  @AttributeOverrides({
+      @AttributeOverride(name="content",column = @Column(table="article_content")),
+      @AttributeOverride(name="contentType",column = @Column(table="article_content"))
+  })
+  private ArticleContent content;
 
 }
