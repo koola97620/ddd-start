2c7ef0000b16cb175683b92efe329bb14811edba
Jaeyong <koola97620@nate.com>
2020-06-27 00:18:38 +0900

    Chapter 04

--- my-ddd/src/main/java/me/jdragon/chapter3/Product.java
+++ my-ddd/src/main/java/me/jdragon/chapter3/Product.java
@@ -1,11 +1,19 @@
 package me.jdragon.chapter3;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Convert;
+import javax.persistence.Converter;
 import javax.persistence.ElementCollection;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
 import javax.persistence.Table;
 
 /**
@@ -20,9 +28,22 @@ public class Product {
   @EmbeddedId
   private ProductId id;
 
+  @Convert(converter = MoneyConverter.class)
+  private Money money;
+
+  @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE},orphanRemoval = true)
+  @JoinColumn(name = "product_id")
+  @OrderColumn(name ="list_idx")
+  private List<Image> images = new ArrayList<>();
+
   @ElementCollection
   @CollectionTable(name = "product_category", joinColumns = @JoinColumn(name = "product_id"))
   private Set<CategoryId> categoryIds;
 
   private CategoryId category;
+
+  public void changeImages(List<Image> newImages) {
+    images.clear();
+    images.addAll(newImages);
+  }
 }
