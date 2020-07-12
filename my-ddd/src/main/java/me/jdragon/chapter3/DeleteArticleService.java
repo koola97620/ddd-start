ca7de11f4984d4ed43a822c04e3d35e3006e267b
Jaeyong <koola97620@nate.com>
2020-07-12 21:47:20 +0900

    Chapter 06

---
+++ my-ddd/src/main/java/me/jdragon/chapter3/DeleteArticleService.java
@@ -0,0 +1,14 @@
+package me.jdragon.chapter3;
+
+/**
+ * @author choijaeyong on 2020/07/12.
+ * @project my-ddd
+ * @description
+ */
+public class DeleteArticleService {
+
+  public void delete(String userId, Long articleId) {
+
+  }
+
+}
